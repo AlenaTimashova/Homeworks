8d70357c898d810099fc2f2aaff05e4419089298
Alena <alyona8808@mail.ru>
2022-10-21 13:59:18 +0300

    Homework 1 via functions

--- Homework1.1/Program.cs
+++ Homework1.1/Program.cs
@@ -71,14 +71,19 @@ else
 
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
-Console.Write("Input a positive integer number: ");
-int number = Convert.ToInt32(Console.ReadLine());
+void EvenRow(int number)
+{
 int currentNumber = 2;
 
 while(currentNumber <= number)
 {
-    if(currentNumber % 2 == 0)
-   { Console.Write(currentNumber + " ");
-   }
+   Console.Write(currentNumber + " ");
    currentNumber = currentNumber+2;
 }
+
+}
+
+Console.Write("Input a positive integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+EvenRow(num);
